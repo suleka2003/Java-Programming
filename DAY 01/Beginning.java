afe8be466ca62095c9321325d6742a9e254c50c7
Suleka Dissanayake <sulekadissanayake2003@gmail.com>
2025-03-29 09:45:11 +0530

    Add files via upload

---
+++ DAY 01/Beginning.java
@@ -0,0 +1,7 @@
+class Beginning {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("This is a simple Java program.");
+        System.out.println("It demonstrates the basic structure of a Java application.");
+    }
+}
