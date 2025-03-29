162cab601a8ccba50963bf8e7a4cba53ac9b306d
Suleka Dissanayake <sulekadissanayake2003@gmail.com>
2025-03-29 21:48:05 +0530

    Update App.java

--- DAY 04/App.java
+++ DAY 04/App.java
@@ -5,9 +5,10 @@ class App {
     public static void main(String[] args) {
         A.Test T1 = new A.Test() ;
         B.Test T2 = new B.Test() ;
-		B.Test T3 = new B.Test() ;
+	B.Test T3 = new B.Test() ;
+	    
         T1.display();
         T2.display();
-		T3.print();
+	T3.print();
     }
 }
