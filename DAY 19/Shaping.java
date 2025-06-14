02469edfd9da5e77a333a0d754f4990c0caee0e5
Suleka Dissanayake <sulekadissanayake2003@gmail.com>
2025-06-14 18:32:35 +0530

    Update Shaping.java

--- DAY 19/Shaping.java
+++ DAY 19/Shaping.java
@@ -1,30 +1,44 @@
 import java.util.*;
 
 abstract class Shape {
+    String name;
+
+    Shape(String name) {
+        this.name = name;
+    }
+
     abstract double calculateArea();
+
+    void display() {
+        System.out.println("Calculating area of: " + name);
+    }
 }
 
 class Square extends Shape {
-	double length;
+    double length;
 
-	Square(double length) {
-		this.length = length;
-	}
+    Square(double length) {
+        super("Square");
+        this.length = length;
+    }
 
-	double calculateArea() {
-		return length * length;
-	}
+    double calculateArea() {
+        super.display();
+        return length * length;
+    }
 }
 
 class Rectangle extends Shape {
     double length, width;
 
     Rectangle(double length, double width) {
+        super("Rectangle");
         this.length = length;
         this.width = width;
     }
 
     double calculateArea() {
+        super.display();
         return length * width;
     }
 }
@@ -33,31 +47,35 @@ class Circle extends Shape {
     double radius;
 
     Circle(double radius) {
+        super("Circle");
         this.radius = radius;
     }
 
     double calculateArea() {
+        super.display();
         return 22.0 / 7 * radius * radius;
     }
 }
 
 class Triangular extends Shape {
-	double length, height;
+    double length, height;
 
-	Triangular(double length, double height) {
-		this.length = length;
-		this.height = height;
-	}
+    Triangular(double length, double height) {
+        super("Triangular");
+        this.length = length;
+        this.height = height;
+    }
 
-	double calculateArea(){
-		return 0.5 * length * height;
-	}
+    double calculateArea() {
+        super.display();
+        return 0.5 * length * height;
+    }
 }
 
 public class Shaping {
     public static void main(String[] args) {
-    	Square s1 = new Square(5);
-    	System.out.println("Area of square " + s1.calculateArea());
+        Square s1 = new Square(5);
+        System.out.println("Area of square: " + s1.calculateArea());
 
         Rectangle r1 = new Rectangle(5, 10);
         System.out.println("Area of rectangle: " + r1.calculateArea());
@@ -66,6 +84,6 @@ public class Shaping {
         System.out.println("Area of circle: " + c1.calculateArea());
 
         Triangular t1 = new Triangular(4, 5);
-        System.out.println("Area of triangular " + t1.calculateArea());
+        System.out.println("Area of triangular: " + t1.calculateArea());
     }
 }
